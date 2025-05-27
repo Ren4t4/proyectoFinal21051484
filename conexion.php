5863bc24ded3b09a1ffba6f9ad7d1b5ed5fcd552
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-05-27 13:00:49 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,12 @@
+
 <?php
 $host = "app-servidor21051484.mysql.database.azure.com";
-$usuario = "app-servidor21051484";
+$usuario = "rootits@app-servidor21051484"; // Debe incluir el nombre del servidor
 $contrasena = "Pi-21051484";
 $base_datos = "calificaciones";
 
-$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+// Crear conexión con SSL
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
