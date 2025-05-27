43718d7b606e418d7bf61ca2a20a7fd1775ac86b
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-05-27 00:27:14 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,4 @@
 <?php
-$host = "40.118.247.100";  // IP privada del servidor MySQL
-$usuario = "rootits";  // Cambia esto por tu usuario de MySQL
-$contrasena = "rootits1";  // Cambia esto por tu contraseña de MySQL
-$base_datos = "calificaciones";
-
 $host = "sqlserver-210512484.mysql.database.azure.com";
 $usuario = "rootits@sqlserver-210512484";
 $contrasena = "Pi-21051484";
