543aa6ed8fa0a6c3782739f21bda829877dd99aa
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-05-27 14:51:12 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ $base_datos = "calificaciones";
 $puerto = 3306;
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $host, $usuario, $contrasena, $base_datos, 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
