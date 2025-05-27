3ce55b45381f5993e6a2a96504e8c8b3b6f11555
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-05-27 14:58:16 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,11 +5,10 @@ $contrasena = "Pi-21051484";
 $base_datos = "calificaciones";
 $puerto = 3306;
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $host, $usuario, $contrasena, $base_datos, 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos, 3306);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
 }
 ?>
 
